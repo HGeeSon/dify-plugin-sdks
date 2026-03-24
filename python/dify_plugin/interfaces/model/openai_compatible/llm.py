493d0e7b993d1b04c381bddff7fca5dfce83efc9
HGeeSon <34162594+HGeeSon@users.noreply.github.com>
2026-03-24 21:01:24 +0800

    Update llm.py
    If the content of the request message for the large model plugin is in Chinese, it will be converted to Unicode. Here is the solution

--- python/dify_plugin/interfaces/model/openai_compatible/llm.py
+++ python/dify_plugin/interfaces/model/openai_compatible/llm.py
@@ -481,11 +481,11 @@ class OAICompatLargeLanguageModel(_CommonOaiApiCompat, LargeLanguageModel):
 
         if user:
             data["user"] = user
-
+        payload = json.dumps(data, ensure_ascii=False).encode('utf-8')
         response = requests.post(
             endpoint_url,
             headers=headers,
-            json=data,
+            json=payload,
             timeout=(10, _plugin_config.MAX_REQUEST_TIMEOUT),
             stream=stream,
         )
